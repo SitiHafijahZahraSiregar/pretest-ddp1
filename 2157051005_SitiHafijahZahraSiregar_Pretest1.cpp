3a43481bb089ed152fcbd564b507854dafe6e8d2
SitiHafijahZahraSiregar <92381404+SitiHafijahZahraSiregar@users.noreply.github.com>
2021-12-15 21:45:58 +0700

    Add files via upload

---
+++ 2157051005_SitiHafijahZahraSiregar_Pretest1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;   
+int main (){
+	/*Nama : Siti Hafijah Zahra Siregar
+	  NPM  : 2157051005
+	  Kelas: B */
+	  
+   //string//
+   string nama ;
+   string alamat ; 
+   string email ;
+   string nomor ;
+
+   //input//
+   cout<< "Masukan nama : ";
+   cin >> nama;
+   cout << "Masukkan alamat : ";
+   cin >> alamat ;
+   cout << "Masukkan email : ";
+   cin >> email ;
+   cout << "Masukkan nomor : ";
+   cin >> nomor ;
+	
+    //output//
+	cout << "Nama : " + nama << endl;
+	cout << "alamat : " + alamat << endl;
+	cout << "email : " + email << endl ;
+	cout << "nomor : ";
+	cout << nomor << endl;
+	
+	return 0;
+	}
